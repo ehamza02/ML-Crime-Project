138f357ada2b037027d2ce5c1f07cc152db36565
ehamza02 <erfanhamza02@gmail.com>
2024-08-07 17:14:58 -0400

    NN class, train, test, feedforward, constructor. Need to fix Input and Output class call in NN class

--- Layer.py
+++ Layer.py
@@ -24,15 +24,25 @@ class Layer:
 
     
 class Input(Layer):
-    def __init__(self, data_dim: int, data):
+    def __init__(self, data_dim: int):
         super().__init__(data_dim)
+        
+
+    def feed_forward(self, data):
         self.data = data
+
+        return self.data
+        
 
 
 class Hidden(Layer):
     def __init__(self, data_dim: int, activation):
         super().__init__(data_dim)
         self.activation = activation
+    def init_gradient(self):
+        self.weights_gradient = np.zeros((self.data_dim, self.prev_layer.data_dim))
+        self.biases_gradient = np.zeros((self.data_dim, 1))
+        
 
 
     def initialize(self):
@@ -58,12 +68,17 @@ class Hidden(Layer):
             deriv_b = self.delta #1(grad of y_hat with respect to b) * grad of mse with respect to y_hat
 
             return deriv_w, deriv_b
+        
 
 
 class Output(Layer):
     def __init__(self, data_dim: int, activation):
         super().__init__(data_dim)
         self.activation = activation
+
+    def init_gradient(self):
+        self.weights_gradient = np.zeros((self.data_dim, self.prev_layer.data_dim))
+        self.biases_gradient = np.zeros((self.data_dim, 1))
 
      
     def initialize(self):
@@ -90,3 +105,99 @@ class Output(Layer):
         self.delta = deriv_yhat_loss
 
         return deriv_w, deriv_b
+    
+class NeuralNetwork:
+    import Layer
+    def  __init__(self, activation, hidden: int, layer_dimensions: list, x, y):
+        if hidden + 2 != len(layer_dimensions):
+            print("invalid length")
+        else:
+            self.activation = activation
+            self.hidden = hidden
+            self.layer_dimensions = layer_dimensions
+            self.x = x
+            self.y = y
+            self.neural_network = []
+            input = Layer.Input(layer_dimensions[0])
+            output = Layer.Output(layer_dimensions[-1], "None")
+            self.neural_network.append(input)
+
+            for i in range(1, len(layer_dimensions) - 2):
+                hidden = Layer.Hidden(layer_dimensions[i], activation)
+                self.neural_network.append(hidden)
+            self.neural_network.append(output)
+
+            self.neural_network[0].set_prev_next_layer(None, self.neural_network[1])
+            
+            for i in range(1, len(self.neural_network) - 2):
+                self.neural_network[i].set_prev_next_layer(self.neural_network[i - 1], self.neural_network[i + 1])
+
+            self.neural_network[-1].set_prev_next_layer(self.neural_network[-2], None)
+
+            for i in range(1, len(self.neural_network) - 1):
+                self.neural_network[i].initialize()
+            self.x_train = self.x[:int(.8 * len(x))]
+            self.y_train = self.y[:int(.8 * len(y))]
+            self.x_test = self.x[-int(.2 * len(x)):]
+            self.y_test = self.y[-int(.2 * len(y)):]
+
+
+
+
+    def feed_forward(self, data):
+        for i in range(1, len(self.neural_network) - 2):
+            self.neural_network[i].feed_forward()
+
+        return self.neural_network[-1].feed_forward()
+    
+    def train(self, learn_rate = 0.001, max_iter = 5000):
+        for i in range(1, max_iter):
+            for j in range(1, len(self.neural_network) - 1):
+                self.neural_network[j].init_gradient()
+            for k in range(0, len(self.x_train) - 1):
+                y_hat = self.feed_forward(self.x_train[k])
+                y = self.y_train[k]
+                call_backprop = self.neural_network[-1].backprop(y)
+                self.neural_network[-1].weights_gradient -= call_backprop[0]
+                self.neural_network[-1].bias_gradient -= call_backprop[1]
+
+                for m in range(len(self.neural_network) - 2, 1):
+                    call_backprop = self.neural_network[m].backprop(y)
+                    self.neural_network[m].weights_gradient -= call_backprop[0]
+                    self.neural_network[m].bias_gradient -= call_backprop[1]
+            for n in range(1, len(self.neural_network) - 1):
+                self.neural_network[n].weights -= learn_rate * self.neural_network[n].weights_gradient
+                self.neural_network[n].bias -= learn_rate * self.neural_network[n].bias_gradient
+        
+    def test(self):
+        loss = 0
+
+        for i in range(0, len(self.x_test) - 1):
+            y_hat = self.feed_forward(self.x_test[i])
+            y = self.y_test[i]
+            loss += (y_hat - y)**2
+        mse = loss/(2 * len(self.x_test))
+        return mse
+    
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+     
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import Layer 
 
 # Loading data
 file_path = "full_data.csv"
@@ -57,7 +58,10 @@ df_selected = df_selected.drop('Police_per_Population', axis=1)
 df_selected = df_selected.query("Violent_Crime_per_Population != '?' and Nonviolent_Crime_per_Population != '?'")
 
 print(df_selected.shape)
+
 # 1902 x 23
 # Input layer: 19 x 1
 # Output layer: 2 x 1 or 1 x 1
 # Number of total examples = 1902 (80/20 train/test split) 
+
+NN = Layer.NeuralNetwork("ReLU", 1, [19, 22, 1], df_selected.iloc[:, 2:21].to_numpy, df_selected.iloc[:, 21:22].to_numpy)
